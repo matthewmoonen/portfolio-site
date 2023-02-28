b27021213f61d2426aa316a4ffb2f07d87ea87f2
Matthew Moonen <86789202+matthewmoonen@users.noreply.github.com>
2023-02-28 16:15:42 +1100

    Added index page

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "<h1 style='color: red'>[ > ]</h1><h3>Hello YouTube</h3>"
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
