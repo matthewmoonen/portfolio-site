d18de84890a452ddfd2f25034f33f2d9de23c39e
Matthew Moonen <86789202+matthewmoonen@users.noreply.github.com>
2023-03-05 21:02:23 +1100

    Revert "Added hover fade to navbar; fixed hover icons disappearing on window resize"
    This reverts commit 0db0f605c54cab4c036d7f318b230016256ebc60.

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -1,30 +1,11 @@
 
-window.onload = addHoverListeners;
-window.onresize = fixDisplayMobileDesktop;
 
+
+window.onresize = fixDisplayMobileDesktop;
+window.onload = fixDisplayMobileDesktop;
 function fixDisplayMobileDesktop() {
     if($(window).width() > 991){
-        // Stop navbar text/icons disappearing on window resize
-        document.querySelector(".navbar-links").style.display = "inline"; 
-    } else {
-
-        toggleMenuOff()
-
-        // Remove hover event listeners
-        document.querySelector("#text-link0").removeEventListener("mouseover", mouseOver0);
-        document.querySelector("#text-link0").removeEventListener("mouseout", mouseOut0);
-        document.querySelector("#text-link0").removeEventListener("mouseover", mouseOver0);
-        document.querySelector("#text-link0").removeEventListener("mouseout", mouseOut0);
-        document.querySelector("#text-link1").removeEventListener("mouseover", mouseOver1);
-        document.querySelector("#text-link1").removeEventListener("mouseout", mouseOut1);
-        document.querySelector("#text-link2").removeEventListener("mouseover", mouseOver2);
-        document.querySelector("#text-link2").removeEventListener("mouseout", mouseOut2);
-        document.querySelector("#icon-link0").removeEventListener("mouseover", mouseOver00);
-        document.querySelector("#icon-link0").removeEventListener("mouseout", mouseOut00);
-        document.querySelector("#icon-link1").removeEventListener("mouseover", mouseOver01);
-        document.querySelector("#icon-link1").removeEventListener("mouseout", mouseOut01);
-        document.querySelector("#icon-link2").removeEventListener("mouseover", mouseOver02);
-        document.querySelector("#icon-link2").removeEventListener("mouseout", mouseOut02);
+        document.querySelector(".navbar-links").style.display = "inline";
     }
 }
 
@@ -80,109 +61,109 @@ function addHoverListeners() {
     // }
 
 
-document.querySelector("#text-link0").addEventListener("mouseover", mouseOver0);
-document.querySelector("#text-link0").addEventListener("mouseout", mouseOut0);
+// document.querySelector("#text-link0").addEventListener("mouseover", mouseOver0);
+// document.querySelector("#text-link0").addEventListener("mouseout", mouseOut0);
 
-document.querySelector("#text-link0").addEventListener("mouseover", mouseOver0);
-document.querySelector("#text-link0").addEventListener("mouseout", mouseOut0);
+// document.querySelector("#text-link0").addEventListener("mouseover", mouseOver0);
+// document.querySelector("#text-link0").addEventListener("mouseout", mouseOut0);
 
-document.querySelector("#text-link1").addEventListener("mouseover", mouseOver1);
-document.querySelector("#text-link1").addEventListener("mouseout", mouseOut1);
+// document.querySelector("#text-link1").addEventListener("mouseover", mouseOver1);
+// document.querySelector("#text-link1").addEventListener("mouseout", mouseOut1);
 
-document.querySelector("#text-link2").addEventListener("mouseover", mouseOver2);
-document.querySelector("#text-link2").addEventListener("mouseout", mouseOut2);
+// document.querySelector("#text-link2").addEventListener("mouseover", mouseOver2);
+// document.querySelector("#text-link2").addEventListener("mouseout", mouseOut2);
 
-document.querySelector("#icon-link0").addEventListener("mouseover", mouseOver00);
-document.querySelector("#icon-link0").addEventListener("mouseout", mouseOut00);
+// document.querySelector("#icon-link0").addEventListener("mouseover", mouseOver00);
+// document.querySelector("#icon-link0").addEventListener("mouseout", mouseOut00);
 
-document.querySelector("#icon-link1").addEventListener("mouseover", mouseOver01);
-document.querySelector("#icon-link1").addEventListener("mouseout", mouseOut01);
+// document.querySelector("#icon-link1").addEventListener("mouseover", mouseOver01);
+// document.querySelector("#icon-link1").addEventListener("mouseout", mouseOut01);
 
-document.querySelector("#icon-link2").addEventListener("mouseover", mouseOver02);
-document.querySelector("#icon-link2").addEventListener("mouseout", mouseOut02);
+// document.querySelector("#icon-link2").addEventListener("mouseover", mouseOver02);
+// document.querySelector("#icon-link2").addEventListener("mouseout", mouseOut02);
 
-function mouseOver0() {
-    document.querySelector("#text-link1").style.color = "var(--hoverColour)";
-    document.querySelector("#text-link2").style.color = "var(--hoverColour)";
-    document.querySelector("#icon-link0").style.fill = "var(--hoverColour)";
-    document.querySelector("#icon-link1").style.fill = "var(--hoverColour)";
-    document.querySelector("#icon-link2").style.fill = "var(--hoverColour)";
-}
-function mouseOut0() {
-    document.querySelector("#text-link1").style.color = "var(--mainColour)";
-    document.querySelector("#text-link2").style.color = "var(--mainColour)";
-    document.querySelector("#icon-link0").style.fill = "var(--mainColour)";
-    document.querySelector("#icon-link1").style.fill = "var(--mainColour)";
-    document.querySelector("#icon-link2").style.fill = "var(--mainColour)";
-}
-function mouseOver1() {
-    document.querySelector("#text-link0").style.color = "var(--hoverColour)";
-    document.querySelector("#text-link2").style.color = "var(--hoverColour)";
-    document.querySelector("#icon-link0").style.fill = "var(--hoverColour)";
-    document.querySelector("#icon-link1").style.fill = "var(--hoverColour)";
-    document.querySelector("#icon-link2").style.fill = "var(--hoverColour)";
-}
-function mouseOut1() {
-    document.querySelector("#text-link0").style.color = "var(--mainColour)";
-    document.querySelector("#text-link2").style.color = "var(--mainColour)";
-    document.querySelector("#icon-link0").style.fill = "var(--mainColour)";
-    document.querySelector("#icon-link1").style.fill = "var(--mainColour)";
-    document.querySelector("#icon-link2").style.fill = "var(--mainColour)";
-}
-function mouseOver2() {
-    document.querySelector("#text-link0").style.color = "var(--hoverColour)";
-    document.querySelector("#text-link1").style.color = "var(--hoverColour)";
-    document.querySelector("#icon-link0").style.fill = "var(--hoverColour)";
-    document.querySelector("#icon-link1").style.fill = "var(--hoverColour)";
-    document.querySelector("#icon-link2").style.fill = "var(--hoverColour)";
-}
-function mouseOut2() {
-    document.querySelector("#text-link0").style.color = "var(--mainColour)";
-    document.querySelector("#text-link1").style.color = "var(--mainColour)";
-    document.querySelector("#icon-link0").style.fill = "var(--mainColour)";
-    document.querySelector("#icon-link1").style.fill = "var(--mainColour)";
-    document.querySelector("#icon-link2").style.fill = "var(--mainColour)";
-}
-function mouseOver00() {
-    document.querySelector("#text-link0").style.color = "var(--hoverColour)";
-    document.querySelector("#text-link1").style.color = "var(--hoverColour)";
-    document.querySelector("#text-link2").style.color = "var(--hoverColour)";
-    document.querySelector("#icon-link1").style.fill = "var(--hoverColour)";
-    document.querySelector("#icon-link2").style.fill = "var(--hoverColour)";
-}
-function mouseOut00() {
-    document.querySelector("#text-link0").style.color = "var(--mainColour)";
-    document.querySelector("#text-link1").style.color = "var(--mainColour)";
-    document.querySelector("#text-link2").style.color = "var(--mainColour)";
-    document.querySelector("#icon-link1").style.fill = "var(--mainColour)";
-    document.querySelector("#icon-link2").style.fill = "var(--mainColour)";
-}
+// function mouseOver0() {
+//     document.querySelector("#text-link1").style.color = "var(--hoverColour)";
+//     document.querySelector("#text-link2").style.color = "var(--hoverColour)";
+//     document.querySelector("#icon-link0").style.fill = "var(--hoverColour)";
+//     document.querySelector("#icon-link1").style.fill = "var(--hoverColour)";
+//     document.querySelector("#icon-link2").style.fill = "var(--hoverColour)";
+// }
+// function mouseOut0() {
+//     document.querySelector("#text-link1").style.color = "var(--mainColour)";
+//     document.querySelector("#text-link2").style.color = "var(--mainColour)";
+//     document.querySelector("#icon-link0").style.fill = "var(--mainColour)";
+//     document.querySelector("#icon-link1").style.fill = "var(--mainColour)";
+//     document.querySelector("#icon-link2").style.fill = "var(--mainColour)";
+// }
+// function mouseOver1() {
+//     document.querySelector("#text-link0").style.color = "var(--hoverColour)";
+//     document.querySelector("#text-link2").style.color = "var(--hoverColour)";
+//     document.querySelector("#icon-link0").style.fill = "var(--hoverColour)";
+//     document.querySelector("#icon-link1").style.fill = "var(--hoverColour)";
+//     document.querySelector("#icon-link2").style.fill = "var(--hoverColour)";
+// }
+// function mouseOut1() {
+//     document.querySelector("#text-link0").style.color = "var(--mainColour)";
+//     document.querySelector("#text-link2").style.color = "var(--mainColour)";
+//     document.querySelector("#icon-link0").style.fill = "var(--mainColour)";
+//     document.querySelector("#icon-link1").style.fill = "var(--mainColour)";
+//     document.querySelector("#icon-link2").style.fill = "var(--mainColour)";
+// }
+// function mouseOver2() {
+//     document.querySelector("#text-link0").style.color = "var(--hoverColour)";
+//     document.querySelector("#text-link1").style.color = "var(--hoverColour)";
+//     document.querySelector("#icon-link0").style.fill = "var(--hoverColour)";
+//     document.querySelector("#icon-link1").style.fill = "var(--hoverColour)";
+//     document.querySelector("#icon-link2").style.fill = "var(--hoverColour)";
+// }
+// function mouseOut2() {
+//     document.querySelector("#text-link0").style.color = "var(--mainColour)";
+//     document.querySelector("#text-link1").style.color = "var(--mainColour)";
+//     document.querySelector("#icon-link0").style.fill = "var(--mainColour)";
+//     document.querySelector("#icon-link1").style.fill = "var(--mainColour)";
+//     document.querySelector("#icon-link2").style.fill = "var(--mainColour)";
+// }
+// function mouseOver00() {
+//     document.querySelector("#text-link0").style.color = "var(--hoverColour)";
+//     document.querySelector("#text-link1").style.color = "var(--hoverColour)";
+//     document.querySelector("#text-link2").style.color = "var(--hoverColour)";
+//     document.querySelector("#icon-link1").style.fill = "var(--hoverColour)";
+//     document.querySelector("#icon-link2").style.fill = "var(--hoverColour)";
+// }
+// function mouseOut00() {
+//     document.querySelector("#text-link0").style.color = "var(--mainColour)";
+//     document.querySelector("#text-link1").style.color = "var(--mainColour)";
+//     document.querySelector("#text-link2").style.color = "var(--mainColour)";
+//     document.querySelector("#icon-link1").style.fill = "var(--mainColour)";
+//     document.querySelector("#icon-link2").style.fill = "var(--mainColour)";
+// }
 
-function mouseOver01() {
-    document.querySelector("#text-link0").style.color = "var(--hoverColour)";
-    document.querySelector("#text-link1").style.color = "var(--hoverColour)";
-    document.querySelector("#text-link2").style.color = "var(--hoverColour)";
-    document.querySelector("#icon-link0").style.fill = "var(--hoverColour)";
-    document.querySelector("#icon-link2").style.fill = "var(--hoverColour)";
-}
-function mouseOut01() {
-    document.querySelector("#text-link0").style.color = "var(--mainColour)";
-    document.querySelector("#text-link1").style.color = "var(--mainColour)";
-    document.querySelector("#text-link2").style.color = "var(--mainColour)";
-    document.querySelector("#icon-link0").style.fill = "var(--mainColour)";
-    document.querySelector("#icon-link2").style.fill = "var(--mainColour)";
-}
-function mouseOver02() {
-    document.querySelector("#text-link0").style.color = "var(--hoverColour)";
-    document.querySelector("#text-link1").style.color = "var(--hoverColour)";
-    document.querySelector("#text-link2").style.color = "var(--hoverColour)";
-    document.querySelector("#icon-link0").style.fill = "var(--hoverColour)";
-    document.querySelector("#icon-link1").style.fill = "var(--hoverColour)";
-}
-function mouseOut02() {
-    document.querySelector("#text-link0").style.color = "var(--mainColour)";
-    document.querySelector("#text-link1").style.color = "var(--mainColour)";
-    document.querySelector("#text-link2").style.color = "var(--mainColour)";
-    document.querySelector("#icon-link0").style.fill = "var(--mainColour)";
-    document.querySelector("#icon-link1").style.fill = "var(--mainColour)";
-}
+// function mouseOver01() {
+//     document.querySelector("#text-link0").style.color = "var(--hoverColour)";
+//     document.querySelector("#text-link1").style.color = "var(--hoverColour)";
+//     document.querySelector("#text-link2").style.color = "var(--hoverColour)";
+//     document.querySelector("#icon-link0").style.fill = "var(--hoverColour)";
+//     document.querySelector("#icon-link2").style.fill = "var(--hoverColour)";
+// }
+// function mouseOut01() {
+//     document.querySelector("#text-link0").style.color = "var(--mainColour)";
+//     document.querySelector("#text-link1").style.color = "var(--mainColour)";
+//     document.querySelector("#text-link2").style.color = "var(--mainColour)";
+//     document.querySelector("#icon-link0").style.fill = "var(--mainColour)";
+//     document.querySelector("#icon-link2").style.fill = "var(--mainColour)";
+// }
+// function mouseOver02() {
+//     document.querySelector("#text-link0").style.color = "var(--hoverColour)";
+//     document.querySelector("#text-link1").style.color = "var(--hoverColour)";
+//     document.querySelector("#text-link2").style.color = "var(--hoverColour)";
+//     document.querySelector("#icon-link0").style.fill = "var(--hoverColour)";
+//     document.querySelector("#icon-link1").style.fill = "var(--hoverColour)";
+// }
+// function mouseOut02() {
+//     document.querySelector("#text-link0").style.color = "var(--mainColour)";
+//     document.querySelector("#text-link1").style.color = "var(--mainColour)";
+//     document.querySelector("#text-link2").style.color = "var(--mainColour)";
+//     document.querySelector("#icon-link0").style.fill = "var(--mainColour)";
+//     document.querySelector("#icon-link1").style.fill = "var(--mainColour)";
+// }
