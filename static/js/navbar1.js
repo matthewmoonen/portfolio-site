9621df23e7f8a5b9e2fd8342c140ee330c0d9ca8
Matthew Moonen <86789202+matthewmoonen@users.noreply.github.com>
2023-03-05 22:30:39 +1100

    Add/remove event listeners somewhat working as expected

--- static/js/navbar1.js
+++ static/js/navbar1.js
@@ -2,16 +2,34 @@
 
 
 window.onresize = fixDisplayMobileDesktop;
-window.onload = fixDisplayMobileDesktop;
+window.onload = addHoverListeners;
 function fixDisplayMobileDesktop() {
     if($(window).width() > 991){
     // Stop navbar text/icons disappearing on window resize
     document.querySelector(".navbar-links").style.display = "inline";
-    addHoverListeners()
 } else {
-        removeHoverListeners()
         toggleMenuOff()
+        addHoverListeners(false)
         // Remove hover event listeners
+
+        document.querySelector("#text-link0").removeEventListener("mouseover", mouseOver0);
+        document.querySelector("#text-link0").removeEventListener("mouseout", mouseOut0);
+        // document.querySelector("#text-link0").removeEventListener("mouseover", mouseOver0);
+        // document.querySelector("#text-link0").removeEventListener("mouseout", mouseOut0);
+        document.querySelector("#text-link1").removeEventListener("mouseover", mouseOver1);
+        document.querySelector("#text-link1").removeEventListener("mouseout", mouseOut1);
+        document.querySelector("#text-link2").removeEventListener("mouseover", mouseOver2);
+        document.querySelector("#text-link2").removeEventListener("mouseout", mouseOut2);
+        document.querySelector("#icon-link0").removeEventListener("mouseover", mouseOver00);
+        document.querySelector("#icon-link0").removeEventListener("mouseout", mouseOut00);
+        document.querySelector("#icon-link1").removeEventListener("mouseover", mouseOver01);
+        document.querySelector("#icon-link1").removeEventListener("mouseout", mouseOut01);
+        document.querySelector("#icon-link2").removeEventListener("mouseover", mouseOver02);
+        document.querySelector("#icon-link2").removeEventListener("mouseout", mouseOut02);
+
+
+
+
 }
 }
 
@@ -32,6 +50,7 @@ function toggleMenuOff() {
     const body = document.querySelector("body")
     body.style.maxHeight = "none"
     body.style.overflow = "scroll"
+
     document.querySelector(".navbar").style.backgroundColor = "hsl(240, 3%, 23%)"
     document.querySelector(".hide-me").style.display = "none"
     document.querySelector(".navbar-links").style.display = "none"
@@ -40,38 +59,23 @@ function toggleMenuOff() {
     document.querySelector(".toggle-off").style.display = "none"
 }
 
-function removeHoverListeners() {
-    // document.querySelector("#text-links").style.color= "black;"
-    document.querySelector("#text-link0").removeEventListener("mouseover", mouseOver0);
-    document.querySelector("#text-link0").removeEventListener("mouseout", mouseOut0);
-    document.querySelector("#text-link1").removeEventListener("mouseover", mouseOver1);
-    document.querySelector("#text-link1").removeEventListener("mouseout", mouseOut1);
-    document.querySelector("#text-link2").removeEventListener("mouseover", mouseOver2);
-    document.querySelector("#text-link2").removeEventListener("mouseout", mouseOut2);
-    document.querySelector("#icon-link0").removeEventListener("mouseover", mouseOver00);
-    document.querySelector("#icon-link0").removeEventListener("mouseout", mouseOut00);
-    document.querySelector("#icon-link1").removeEventListener("mouseover", mouseOver01);
-    document.querySelector("#icon-link1").removeEventListener("mouseout", mouseOut01);
-    document.querySelector("#icon-link2").removeEventListener("mouseover", mouseOver02);
-    document.querySelector("#icon-link2").removeEventListener("mouseout", mouseOut02);
-}
-
 function addHoverListeners() {
-    document.querySelector("#text-link0").addEventListener("mouseover", mouseOver0);
-    document.querySelector("#text-link0").addEventListener("mouseout", mouseOut0);
-    document.querySelector("#text-link0").addEventListener("mouseover", mouseOver0);
-    document.querySelector("#text-link0").addEventListener("mouseout", mouseOut0);
-    document.querySelector("#text-link1").addEventListener("mouseover", mouseOver1);
-    document.querySelector("#text-link1").addEventListener("mouseout", mouseOut1);
-    document.querySelector("#text-link2").addEventListener("mouseover", mouseOver2);
-    document.querySelector("#text-link2").addEventListener("mouseout", mouseOut2);
-    document.querySelector("#icon-link0").addEventListener("mouseover", mouseOver00);
-    document.querySelector("#icon-link0").addEventListener("mouseout", mouseOut00);
-    document.querySelector("#icon-link1").addEventListener("mouseover", mouseOver01);
-    document.querySelector("#icon-link1").addEventListener("mouseout", mouseOut01);
-    document.querySelector("#icon-link2").addEventListener("mouseover", mouseOver02);
-    document.querySelector("#icon-link2").addEventListener("mouseout", mouseOut02);
-}
+
+document.querySelector("#text-link0").addEventListener("mouseover", mouseOver0);
+document.querySelector("#text-link0").addEventListener("mouseout", mouseOut0);
+document.querySelector("#text-link0").addEventListener("mouseover", mouseOver0);
+document.querySelector("#text-link0").addEventListener("mouseout", mouseOut0);
+document.querySelector("#text-link1").addEventListener("mouseover", mouseOver1);
+document.querySelector("#text-link1").addEventListener("mouseout", mouseOut1);
+document.querySelector("#text-link2").addEventListener("mouseover", mouseOver2);
+document.querySelector("#text-link2").addEventListener("mouseout", mouseOut2);
+document.querySelector("#icon-link0").addEventListener("mouseover", mouseOver00);
+document.querySelector("#icon-link0").addEventListener("mouseout", mouseOut00);
+document.querySelector("#icon-link1").addEventListener("mouseover", mouseOver01);
+document.querySelector("#icon-link1").addEventListener("mouseout", mouseOut01);
+document.querySelector("#icon-link2").addEventListener("mouseover", mouseOver02);
+document.querySelector("#icon-link2").addEventListener("mouseout", mouseOut02);
+
 
 function mouseOver0() {
     document.querySelector("#text-link1").style.color = "var(--hoverColour)";
@@ -157,4 +161,5 @@ function mouseOut02() {
     document.querySelector("#text-link2").style.color = "var(--mainColour)";
     document.querySelector("#icon-link0").style.fill = "var(--mainColour)";
     document.querySelector("#icon-link1").style.fill = "var(--mainColour)";
+}
 }
