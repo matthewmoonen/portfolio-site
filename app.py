a4298915c8513fb38ba8b5a9c359105ed58cdedc
Matthew Moonen <86789202+matthewmoonen@users.noreply.github.com>
2023-03-08 00:30:09 +1100

    page_id to post_id

--- app.py
+++ app.py
@@ -164,7 +164,7 @@ def code():
 @login_required
 def delete_post(post_id):
     post_id = str(post_id)
-    return f"<h1>{page_id}</h1>"
+    return f"<h1>{post_id}</h1>"
 
     # post = BlogPost.query.filter_by(id=post_id).first_or_404()
     # try:
