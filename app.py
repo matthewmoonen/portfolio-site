05b2eeabaef1c14221d6aaa6020005334c440513
Matthew Moonen <86789202+matthewmoonen@users.noreply.github.com>
2023-03-08 02:07:58 +1100

    Simplified blog page

--- app.py
+++ app.py
@@ -70,19 +70,10 @@ def admin():
 # >>> peter.email
 # u'peter@example.org'
 
-@app.route('/blog/<int:post_id>')
-def show_blog_post(post_id):
+@app.route('/blog/')
+def show_blog_post():
     posts = db.session.query(BlogPost).all()
-    post_id = post_id
-    # post = db.query.filter_by(_id=post_id).first()
-    # return f"{post._id} is the id {post.title} is the title {post.body} is the body."
-
-    return render_template("blog-post.html", posts=posts, post_id=post_id)
-
-
-    # post = BlogPost.query.get_or_404(post_id)
-    # # posts = db.session.query(BlogPost).all()
-    # return render_template('blog-post.html', post=post)
+    return render_template("blog-post.html", posts=posts)
 
 
 
