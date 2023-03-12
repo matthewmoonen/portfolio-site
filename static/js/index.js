a9c08c7e9e5d38dc4a34ab7bdd8954543efb8ce9
Matthew Moonen <matthewmoonen@gmail.com>
2023-03-12 18:11:31 +0000

    Welcome page adnimation complete

---
+++ static/js/index.js
@@ -0,0 +1,156 @@
+
+// var textArray = ['one \n one1', 'two', 'three', 'four']
+// var textArray = ['<span class="red line1">const</span> <span class="darkblue line1">me</span> <span class="red line1">=</span> <span class="blue line1">"Matthew Moonen"</span><span class="white line1">;</span>', '\n<span class="red line2">let</span> <span class="white line2">you</span> <span class="red line2">=</span> <span class="blue line2">"visitor"</span><span class="white line2">;</span>', '\n\n<span class="purple line3">greetings</span><span class="darkblue line3">()</span><span class="white line3">;</span>', '\n\n<span class="red italic-code line4">async function</span> <span class="purple line4">greetings</span><span class="darkblue line4">() {</span>\n<span class="grey line5">••</span><span class="darkblue line5">console</span><span class="white line5">.</span><span class="purple line5">log</span><span class="orange line5">(</span><span class="blue line5">`Welcome</span> <span class="purple line5">${</span><span class="darkblue line5">you</span><span class="purple line5">}</span><span class="blue line5">!`</span><span class="orange line5">)</span><span class="white line5">;</span>\n<span class="grey line6">••</span><span class="red italic-code line6">await new</span> <span class="darkblue line6">Promise</span><span class="orange line6">(</span><span class="purple line6">(resolve)</span> <span class="red line6">=></span> <span class="purple line6">{</span>\n<span class="grey line7">••••</span><span class="darkblue line7">window</span><span class="white line7">.</span><span class="purple line7">addEventListener</span><span class="darkblue line7">(</span><span class="blue line7">"scroll"</span><span class="white line7">,</span> <span class="orange line7">()</span> <span class="red line7">=></span> <span class="orange line7">{</span>\n<span class="grey line8">••••••</span><span class="red line8">const</span> <span class="purple line8">{</span>\n<span class="grey line9">••••••••</span><span class="darkblue line9">scrollTop</span><span class="white line9">,</span>\n<span class="grey line9">••••••••</span><span class="darkblue line9">scrollHeight</span><span class="white line9">,</span>\n<span class="grey line9">••••••••</span><span class="darkblue line9">clientHeight</span>\n<span class="grey line8">••••••</span><span class="purple line8">}</span> <span class="red line8">=</span> <span class="darkblue line8">document</span><span class="white line']
+// var textArray = ['<span class="red line1">const</span> <span class="darkblue line1">me</span> <span class="red line1">=</span> <span class="blue line1">"Matthew Moonen"</span><span class="white line1">;</span>', '\n<span class="red line2">let</span> <span class="white line2">you</span> <span class="red line2">=</span> <span class="blue line2">"visitor"</span><span class="white line2">;</span>', '\n\n<span class="purple line3">greetings</span><span class="darkblue line3">()</span><span class="white line3">;</span>', '\n\n<span class="red italic-code line4">async function</span> <span class="purple line4">greetings</span><span class="darkblue line4">() {</span>\n<span class="grey line5">••</span><span class="darkblue line5">console</span><span class="white line5">.</span><span class="purple line5">log</span><span class="orange line5">(</span><span class="blue line5">`Welcome</span> <span class="purple line5">${</span><span class="darkblue line5">you</span><span class="purple line5">}</span><span class="blue line5">!`</span><span class="orange line5">)</span><span class="white line5">;</span>\n<span class="grey line6">••</span><span class="red italic-code line6">await new</span> <span class="darkblue line6">Promise</span><span class="orange line6">(</span><span class="purple line6">(resolve)</span> <span class="red line6">=></span> <span class="purple line6">{</span>\n<span class="grey line7">••••</span><span class="darkblue line7">window</span><span class="white line7">.</span><span class="purple line7">addEventListener</span><span class="darkblue line7">(</span><span class="blue line7">"scroll"</span><span class="white line7">,</span> <span class="orange line7">()</span> <span class="red line7">=></span> <span class="orange line7">{</span>\n<span class="grey line8">••••••</span><span class="red line8">const</span> <span class="purple line8">{</span>\n<span class="grey line9">••••••••</span><span class="darkblue line9">scrollTop</span><span class="white line9">,</span>\n<span class="grey line10">••••••••</span><span class="darkblue line10">scrollHeight</span><span class="white line10">,</span>\n<span class="grey line11">••••••••</span><span class="darkblue line11">clientHeight</span>\n<span class="grey line8">••••••</span><span class="purple line8">}</span> <span class="red line8">=</span> <span class="darkblue line']
+// var textArray = ['<span class="red line1">const</span> <span class="darkblue line1">me</span> <span class="red line1">=</span> <span class="blue line1">"Matthew Moonen"</span><span class="white line1">;</span>', '\n<span class="red line2">let</span> <span class="white line2">you</span> <span class="red line2">=</span> <span class="blue line2">"visitor"</span><span class="white line2">;</span>', '\n\n<span class="purple line3">greetings</span><span class="darkblue line3">()</span><span class="white line3">;</span>', '\n\n<span class="red italic-code line4">async function</span> <span class="purple line4">greetings</span><span class="darkblue line4">() {</span>\n<span class="grey line5">••</span><span class="darkblue line5">console</span><span class="white line5">.</span><span class="purple line5">log</span><span class="orange line5">(</span><span class="blue line5">`Welcome</span> <span class="purple line5">${</span><span class="darkblue line5">you</span><span class="purple line5">}</span><span class="blue line5">!`</span><span class="orange line5">)</span><span class="white line5">;</span>\n<span class="grey line6">••</span><span class="red italic-code line6">await new</span> <span class="darkblue line6">Promise</span><span class="orange line6">(</span><span class="purple line6">(resolve)</span> <span class="red line6">=></span> <span class="purple line6">{</span>\n<span class="grey line7">••••</span><span class="darkblue line7">window</span><span class="white line7">.</span><span class="purple line7">addEventListener</span><span class="darkblue line7">(</span><span class="blue line7">"scroll"</span><span class="white line7">,</span> <span class="orange line7">()</span> <span class="red line7">=></span> <span class="orange line7">{</span>\n<span class="grey line8">••••••</span><span class="red line8">const</span> <span class="purple line8">{</span>\n<span class="grey line9">••••••••</span><span class="darkblue line9">scrollTop</span><span class="white line9">,</span>\n<span class="grey line10">••••••••</span><span class="darkblue line10">scrollHeight</span><span class="white line10">,</span>\n<span class="grey line11">••••••••</span><span class="darkblue line11">clientHeight</span>\n<span class="grey line12">••••••</span><span class="purple line12">}</span> <span class="red line12">=</span> <span class="darkblue line12">document</span']
+// var textArray = ['<span class="red line1">const</span> <span class="darkblue line1">me</span> <span class="red line1">=</span> <span class="blue line1">"Matthew Moonen"</span><span class="white line1">;</span>', '\n<span class="red line2">let</span> <span class="white line2">you</span> <span class="red line2">=</span> <span class="blue line2">"visitor"</span><span class="white line2">;</span>', '\n\n<span class="purple line3">greetings</span><span class="darkblue line3">()</span><span class="white line3">;</span>', '\n\n<span class="red italic-code line4">async function</span> <span class="purple line4">greetings</span><span class="darkblue line4">() {</span>\n<span class="grey line5">••</span><span class="darkblue line5">console</span><span class="white line5">.</span><span class="purple line5">log</span><span class="orange line5">(</span><span class="blue line5">`Welcome</span> <span class="purple line5">${</span><span class="darkblue line5">you</span><span class="purple line5">}</span><span class="blue line5">!`</span><span class="orange line5">)</span><span class="white line5">;</span>\n<span class="grey line6">••</span><span class="red italic-code line6">await new</span> <span class="darkblue line6">Promise</span><span class="orange line6">(</span><span class="purple line6">(resolve)</span> <span class="red line6">=></span> <span class="purple line6">{</span>\n<span class="grey line7">••••</span><span class="darkblue line7">window</span><span class="white line7">.</span><span class="purple line7">addEventListener</span><span class="darkblue line7">(</span><span class="blue line7">"scroll"</span><span class="white line7">,</span> <span class="orange line7">()</span> <span class="red line7">=></span> <span class="orange line7">{</span>\n<span class="grey line8">••••••</span><span class="red line8">const</span> <span class="purple line8">{</span>\n<span class="grey line9">••••••••</span><span class="darkblue line9">scrollTop</span><span class="white line9">,</span>\n<span class="grey line10">••••••••</span><span class="darkblue line10">scrollHeight</span><span class="white line10">,</span>\n<span class="grey line11">••••••••</span><span class="darkblue line11">clientHeight</span>\n<span class="grey line12">••••••</span><span class="purple line12">}</span> <span class="red line12">=</span> <span class="darkblue line12">document</span><span class']
+var textArray = ['<span class="red line1">const</span> <span class="darkblue line1">me</span> <span class="red line1">=</span> <span class="blue line1">"Matthew Moonen"</span><span class="white line1">;</span>', '\n<span class="red line2">let</span> <span class="white line2">you</span> <span class="red line2">=</span> <span class="blue line2">"visitor"</span><span class="white line2">;</span>', '\n\n<span class="purple line3">greetings</span><span class="darkblue line3">()</span><span class="white line3">;</span>', '\n\n<span class="red italic-code line4">async function</span> <span class="purple line4">greetings</span><span class="darkblue line4">() {</span>\n<span class="grey line5">••</span><span class="darkblue line5">console</span><span class="white line5">.</span><span class="purple line5">log</span><span class="orange line5">(</span><span class="blue line5">`Welcome</span> <span class="purple line5">${</span><span class="darkblue line5">you</span><span class="purple line5">}</span><span class="blue line5">!`</span><span class="orange line5">)</span><span class="white line5">;</span>\n<span class="grey line6">••</span><span class="red italic-code line6">await new</span> <span class="darkblue line6">Promise</span><span class="orange line6">(</span><span class="purple line6">(resolve)</span> <span class="red line6">=></span> <span class="purple line6">{</span>\n<span class="grey line7">••••</span><span class="darkblue line7">window</span><span class="white line7">.</span><span class="purple line7">addEventListener</span><span class="darkblue line7">(</span><span class="blue line7">"scroll"</span><span class="white line7">,</span> <span class="orange line7">()</span> <span class="red line7">=></span> <span class="orange line7">{</span>\n<span class="grey line8">••••••</span><span class="red line8">const</span> <span class="purple line8">{</span>\n<span class="grey line9">••••••••</span><span class="darkblue line9">scrollTop</span><span class="white line9">,</span>\n<span class="grey line10">••••••••</span><span class="darkblue line10">scrollHeight</span><span class="white line10">,</span>\n<span class="grey line11">••••••••</span><span class="darkblue line11">clientHeight</span>\n<span class="grey line12">••••••</span><span class="purple line12">}</span> <span class="red line12">=</span> <span class="darkblue line12">document</span><span class="white line12">.</span><span class="darkblue line12">documentElement</span><span class="white line12">;</span>\n<span class="grey line13">••••••</span><span class="red line13">if</span> <span class="purple line13">(</span><span class="darkblue line13">scrollTop</span> <span class="red line13">+</span> <span class="darkblue line13">clientHeight</span> <span class="red line13">=></span> <span class="darkblue line13">scrollHeight</span><span class="purple line13">) {</span>\n<span class="grey line14">••••••••</span><span class="purple line14">resolve</span><span class="darkblue line14">()</span><span class="white line14">;</span>\n<span class="grey line15">••••••</span><span class="purple line15">}</span>\n<span class="grey line16">••••</span><span class="orange line16">}</span><span class="darkblue line16">)</span><span class="white line16">;</span>\n<span class="grey line17">••</span><span class="purple line17">}</span><span class="orange line17">)</span><span class="white line17">;</span>\n<span class="grey line18">••</span><span class="darkblue line18">console</span><span class="white line18">.</span><span class="purple line18">log</span><span class="orange line18">(</span><span class="blue line18">Thanks for stopping by</span> <span class="purple line18">${</span><span class="darkblue line18">me</span><span class="purple line18">}</span><span class="blue line18">\'s website!</span><span class="orange line18">)</span><span class="white line18">;</span>\n<span class="darkblue line19">}</span>']
+
+
+var element = document.getElementById("text");
+var elementCount = 0;
+
+addElements();
+
+function addElements() {
+    var text = textArray[elementCount].replace(/\n/g, "<br>");
+  var text = textArray[elementCount];
+  var index = 0;
+  var interval;
+  var waitTime;
+  var homeTopHalf = document.querySelector("#text")
+  var fadeOut = false
+
+  if (elementCount <= 2) {
+    waitTime = 1500;
+    interval = setInterval(addLetter, 30);
+  } else if (elementCount === 2) {
+    waitTime = 400;
+    // fadeOut = true;
+    interval = setInterval(addLetter, 15);
+} else if (elementCount === 3) {
+    waitTime = 0;
+    // fadeOut = true;
+    interval = setInterval(addLetter, 15);
+  } 
+
+  function addLetter() {
+    // Get the current character
+    var char = text.charAt(index);
+
+    // Check if the current character is the start of a tag
+    if (char === "<") {
+      // Get the end of the tag
+      var endIndex = text.indexOf(">", index);
+      if (endIndex !== -1) {
+        // Extract the tag and its contents
+        var tag = text.slice(index, endIndex + 1);
+        var contents = text.slice(index + tag.length, endIndex);
+
+        // Create a new element and add it to the HTML element
+        var newElement = document.createElement("span");
+        if (tag.includes("class")) {
+          // Set the class attribute if present
+          var className = tag.match(/class=['"]([^'"]+)['"]/)[1];
+          newElement.setAttribute("class", className);
+        }
+        newElement.textContent = contents;
+        element.appendChild(newElement);
+
+        // Increment the index past the tag and its contents
+        index = endIndex + 1;
+      }
+    } else {
+      // Add the current character to the HTML element
+      var lastChild = element.lastChild;
+      if (lastChild && lastChild.nodeType === Node.ELEMENT_NODE && lastChild.tagName === "SPAN") {
+        // Add the current character to the last span element
+        lastChild.textContent += char;
+      } else {
+        // Create a new span element and add it to the HTML element
+        var newElement = document.createElement("span");
+        newElement.textContent = char;
+        element.appendChild(newElement);
+      }
+
+      // Increment the index
+      index++;
+    }
+
+
+    
+    element.scrollTop = element.scrollHeight;
+    // If all letters have been added, stop the interval and add the cursor
+    if (index >= text.length) {
+        clearInterval(interval);
+        element.insertAdjacentHTML("beforeend", '<span class="cursor">|</span>');
+        // Remove the cursor after 5 seconds
+        setTimeout(function() {
+            var cursor = element.querySelector('.cursor');
+            cursor.parentNode.removeChild(cursor);
+            // if (endAnimation === true) {
+                //     triggerEndAnimation();
+                // }
+                if (fadeOut === true) {
+            triggerFadeOut(homeTopHalf);
+        }
+        elementCount++;
+        addElements();
+      }, waitTime);
+    }
+  }
+}
+
+
+
+function triggerFadeOut(element) {
+    let opacity = 1;
+    function animate() {
+      opacity -= 0.1;
+      element.style.opacity = opacity;
+      if(opacity <= 0) {
+        element.style.display = 'none';
+      } else {
+        requestAnimationFrame(animate);
+      }
+    }
+    requestAnimationFrame(animate);
+  }
+  
+
+
+
+// function triggerFadeOut(topHalfHome) {
+//     let opacity = 1;
+//     const timer = setInterval(function() {
+//       if(opacity <= 0.1) {
+//         clearInterval(timer);
+//         topHalfHome.style.opacity = 0;
+//         topHalfHome.style.display = 'none';
+//       }
+//       topHalfHome.style.opacity = opacity;
+//       opacity -= 0.1;
+//     }, 10);
+//   }
+  
+
+
+// function triggerFadeOut(topHalfHome) {
+//   let opacity = 1;
+//   const timer = setInterval(function() {
+//     if(opacity <= 0.1) {
+//       clearInterval(timer);
+//       topHalfHome.style.display = 'none';
+//     }
+//     topHalfHome.style.opacity = opacity;
+//     opacity -= 0.1;
+//   }, 50);
+// }
+
+function triggerEndAnimation() {
+    setTimeout(() => {
+    const div = document.getElementById("text");
+    div.style.transition = "transform 5s ease-out";
+    div.style.transform = "translateY(-1500px)";
+    }, 5000);
+}
